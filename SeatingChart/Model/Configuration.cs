0a8ebd55ca1a7f144f2d0300e7b6eac1d1cc0cd5
Andrew Burgess <andrew@deceptacle.com>
2012-08-12 21:13:35 -0400

    Added some models and packages

---
+++ SeatingChart/Model/Configuration.cs
@@ -0,0 +1,8 @@
+namespace SeatingChart.Model
+{
+    public class Configuration
+    {
+        public int NumberOfTables { get; set; }
+        public int PeoplePerTable { get; set; }
+    }
+}
---
+++ SeatingChart/Model/Person.cs
@@ -0,0 +1,7 @@
+namespace SeatingChart.Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
