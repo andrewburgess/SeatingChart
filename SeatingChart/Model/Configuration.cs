eec409994411a7b1f01f90eefdea98ebf999b652
Andrew Burgess <andrew@deceptacle.com>
2012-08-12 21:31:11 -0400

    Reads from an input JSON file

--- SeatingChart/Model/Configuration.cs
+++ SeatingChart/Model/Configuration.cs
@@ -1,8 +1,17 @@
+using System.Collections.Generic;
+
 namespace SeatingChart.Model
 {
     public class Configuration
     {
         public int NumberOfTables { get; set; }
         public int PeoplePerTable { get; set; }
+
+        public List<Person> People { get; set; }
+
+        public Configuration()
+        {
+            People = new List<Person>();
+        }
     }
 }
--- SeatingChart/Program.cs
+++ SeatingChart/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
+using SeatingChart.Model;
 
 namespace SeatingChart
 {
@@ -9,6 +12,12 @@ namespace SeatingChart
     {
         static void Main(string[] args)
         {
+            var jsSerializer = new Newtonsoft.Json.JsonSerializer();
+            var textReader = new StreamReader("input.json");
+            var reader = new JsonTextReader(textReader);
+            var cfg = jsSerializer.Deserialize<Configuration>(reader);
+
+            Console.ReadLine();
         }
     }
 }
